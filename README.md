fb0c5c157a0c62e087084e526fcd1187e513d15e
Josiah Campbell <campbjos@mail.gvsu.edu>
2015-08-07 23:26:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+What Is This?
+-------------
+
+This script uses the [requests](http://www.python-requests.org/en/latest/)
+library as well as the
+[webrowser](https://docs.python.org/3/library/webbrowser.html) module
+to make a respectful get http request to reddit's
+(random)[http://reddit.com/r/random] page.
+
+The script asks for a user input on how many random subreddit links to gather.
+It only retrieves successful redirects, and opens them simultaneously
+once they've been gathered.
+
+How To Use The Script
+-----------------------
+
+Assuming you have Python 3.+, all you need to do from your terminal is execute
+the following line:
+``python random_reddit_grabber.py``
+
+The interface will take you the rest of the way.
+
+Installation
+-----------------------
+
+1. Installation is twofold. Make sure you have Python 3.+ and
+2. From your command line, download the Requests module
+``pip install requests``
+
+License
+--------------------------
+
+No license. Try this script out at your leisure.
+
+TODO
+--------------------------
+
+HTML/txt exporting, alternate ways to view finished, gathered links. E.g. avoid
+opening 10+ tabs at once.
---
+++ random_reddit_grabber.py
@@ -0,0 +1,58 @@
+from time import sleep
+import requests
+import sys
+import webbrowser
+
+_MIN_WAIT_TIME = float(0)
+_TUP_STATUS = 0
+_TUP_URL = 1
+_TUP_HEADER_RETRY_AFTER = 2
+
+def __get_random_url(sesh):
+    r = sesh.get('http://reddit.com/r/random/', allow_redirects=True)
+    return r.status_code, r.url, r.headers.get('Retry-After', _MIN_WAIT_TIME)
+
+def __session_loop_rand(expected_count, a_set):
+    s = requests.Session()
+    wait_time = _MIN_WAIT_TIME
+    entry_iter = 0
+    current_count = expected_count
+    while current_count > 0:
+        sleep(wait_time)
+        resultTup = __get_random_url(s)
+        wait_time = float(resultTup[_TUP_HEADER_RETRY_AFTER])
+        current_percent = (1-(current_count/expected_count))*100
+        sys.stdout.write("\r%d%%" % current_percent)
+        sys.stdout.flush()
+        if "/r/random/" not in resultTup[_TUP_URL]:
+            current_count-=1
+            entry_iter+=1
+            a_set.add(resultTup[_TUP_URL])
+
+def __user_amount(attempts):
+    if attempts > 3:
+        print("Are you serious?")
+    user_input = input('How many random results do you want? ')
+    try:
+        user_input = int(user_input)
+        return user_input
+    except ValueError:
+        print("Expected number value. Try again.")
+        user_input = int(0)
+        __user_amount(attempts+1)
+
+def __open_result_tabs(final_set):
+    for found_url in final_set:
+        webbrowser.open_new_tab(found_url)
+
+def main():
+    request_amount = __user_amount(0)
+    my_set = set()
+    __session_loop_rand(request_amount, my_set)
+    sys.stdout.write("\r100%")
+    sys.stdout.flush()
+    print("\nOpening tabs...")
+    __open_result_tabs(my_set)
+
+if __name__ == '__main__':
+    main()
